7c3b3780cf31c2f7e80ca3f90b75d95649fd0334
Kevin Sogata <kevinsogata@gmail.com>
2019-03-23 16:48:27 -0700

    added login

---
+++ src/components/login.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { Button, Form } from 'react-bootstrap';
+
+export default class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: ''
+        };
+    }
+
+    render() {
+        return (
+            <div className="Login">
+            <Form>
+              <Form.Group controlId="email" bsSize="large">
+                <Form.Control
+                  autoFocus
+                  type="email"
+                  value={this.state.email}
+                //   onChange={this.handleChange}
+                />
+              </Form.Group>
+              <Form.Group controlId="password" bsSize="large">
+                <Form.Control
+                  value={this.state.password}
+                //   onChange={this.handleChange}
+                  type="password"
+                />
+              </Form.Group>
+              <Button
+                block
+                bsSize="large"
+                // disabled={!this.validateForm()}
+                type="submit"
+              >
+                Login
+              </Button>
+            </Form>
+          </div>
+        )
+    }
+};
