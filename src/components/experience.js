0f0bc5d09cbb242ae450aad37dcac0bf8bb0f62e
Kevin Sogata <kevinsogata@gmail.com>
2019-03-24 10:44:01 -0700

    magic post baby

--- src/components/experience.js
+++ src/components/experience.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import CreatableSelect from 'react-select/lib/Creatable';
 import { Container, Row, Col, Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import axios from 'axios';
+import qs from 'qs';
 
 const styles = {
     banner: {
@@ -14,20 +16,66 @@ const labelOptions = [
   { value: 't03', label: 'Netowrking' }
 ];
 
+const headers = {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Accept': 'application/json'
+};
+
 class Experience extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            name: '',
-            description: '',
-            address1: '',
-            address2: '',
+            experience_name: '',
+            desc: '',
+            addr1: '',
+            addr2: '',
             city: '',
             state: '',
             zip: '',
-            eventtags: '',
+            eventtags: [],
             capacity: ''
         }
+    }
+
+    onClick  = (e) => {
+        e.preventDefault();
+        console.log(this.state);
+        console.log(decodeURI(qs.stringify(this.state)));
+        let tags = JSON.stringify(this.state.eventtags);
+        let {experience_name, desc, addr1, addr2, city, state, zip, capacity} = this.state;
+        let body = qs.stringify({
+            experience_name,
+            desc,
+            addr1,
+            addr2,
+            city,
+            state,
+            zip,
+            capacity,
+            eventtags: tags
+        })
+        
+        axios.post("http://localhost:5000/api/event", body, {headers})
+            .then((res) => {
+                console.log(res);
+            })
+            .catch((error) => {
+                if (error.response) {
+                    // The request was made and the server responded with a status code
+                    // that falls out of the range of 2xx
+                    console.log(error.response.data);
+                    console.log(error.response.status);
+                    console.log(error.response.headers);
+                  } else if (error.request) {
+                    // The request was made but no response was received
+                    // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+                    // http.ClientRequest in node.js
+                    console.log(error.request);
+                  } else {
+                    // Something happened in setting up the request that triggered an Error
+                    console.log('Error', error.message);
+                  }
+            });
     }
 
     handleChange = (e) => {
@@ -53,12 +101,12 @@ class Experience extends Component {
             <Col>
               <Form>
               <FormGroup>
-                <Label for="expName">Experience Name</Label>
-                <Input onChange={this.handleChange} type="expName" name="expName" id="expName" value={this.state.name}/>
+                <Label for="experience_name">Experience Name</Label>
+                <Input onChange={this.handleChange} type="text" name="experience_name" id="expName" value={this.state.experience_name}/>
               </FormGroup>
               <FormGroup>
-                <Label for="description">Description</Label>
-                <Input onChange={this.handleChange} type="textarea" name="description" id="description" value={this.state.description}/>
+                <Label for="desc">Desc</Label>
+                <Input onChange={this.handleChange} type="textarea" name="desc" id="desc" value={this.state.desc}/>
               </FormGroup>
               </Form>
               <Row form>
@@ -83,12 +131,12 @@ class Experience extends Component {
             <Col>
               <Form>
               <FormGroup>
-                <Label for="address">Address</Label>
-              <Input onChange={this.handleChange} type="text" name="address" id="address" placeholder="1234 Main St" value={this.state.address1}/>
+                <Label for="addr1">Address</Label>
+              <Input onChange={this.handleChange} type="text" name="addr1" id="addr1" placeholder="1234 Main St" value={this.state.addr1}/>
               </FormGroup>
               <FormGroup>
-                <Label for="address2">Address 2</Label>
-                <Input onChange={this.handleChange} type="text" name="address2" id="address2" placeholder="Apartment, studio, or floor" value={this.state.address2}/>
+                <Label for="addr2">Address 2</Label>
+                <Input onChange={this.handleChange} type="text" name="addr2" id="addr2" placeholder="Apartment, studio, or floor" value={this.state.addr2}/>
               </FormGroup>
               <Row form>
                 <Col md={6}>
@@ -115,7 +163,12 @@ class Experience extends Component {
         </Row>
         <Row>
           <Col>
-            <Button color="primary">Submit</Button><br/>
+            <Button 
+                color="primary"
+                onClick={this.onClick}
+            >
+                Submit
+            </Button><br/>
           </Col>
         </Row>
       </Container>
