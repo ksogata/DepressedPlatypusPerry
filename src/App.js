a1902d368e54d5f1ef7b17c9e6ac73bcb83a4fb5
Kevin Sogata <kevinsogata@gmail.com>
2019-03-23 17:29:14 -0700

    react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
+import { Link, Route } from "react-router-dom";
 import Login from './components/login';
-import logo from './logo.svg';
+import Dashboard from './components/dashboard';
+import About from './components/about';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
+        <nav>
+          <Link to="/dashboard">Dashboard</Link>
+          <Link to="/about">About</Link>
+        </nav>
+        <div>
+          <Route path="/dashboard" component={Dashboard} />
+          <Route path="/about" component={About} />
+        </div>
         <Login></Login>
       </div>
     );
