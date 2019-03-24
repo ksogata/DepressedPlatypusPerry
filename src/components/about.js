aee442f10c15b55552334583fbe33c2a824b6ec2
Annie Liu <annieliu@colourchaos.com>
2019-03-24 08:34:14 -0700

    wrote about page blurb

--- src/components/about.js
+++ src/components/about.js
@@ -12,7 +12,8 @@ const About = () => {
       <div>
         <h1 style={styles.banner}>About</h1>
         <Container>
-          <p></p>
+          <p>Having positive social interactions is important for our health and well-being. Social isolation, on the other hand, is associated with many negative health outcomes, and some studies have suggested that the impact of isolation and loneliness is just as high as risk factors such as high blood pressure, obesity, and smoking.</p>
+          <p>[app name here] is designed to help connect people who share similar interests, with a focus on mental health and providing that human connection with others who may have experienced similar situations. We hope that through encouraging people to go out and do activities with other like-minded individuals, this will help increase meaningful social interactions and have a positive influence on people who may be struggling with isolation and loneliness.</p>
         </Container>
       </div>
     )
